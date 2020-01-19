0cb550650ffba8fb28bc522073e190ee61c24b28
igor <Igor Barbosa Nogueira>
2020-01-19 00:00:00 -0300

    Adaptando template e listando notícias do banco

--- portal/views.py
+++ portal/views.py
@@ -1,9 +1,13 @@
 from django.shortcuts import render
 from .models import Noticia
+from django.http import JsonResponse, HttpResponse
+from django.core import serializers
+from django.http import HttpResponse
 
 
 # Create your views here.
 
 def noticias_listar(request):
     noticias = Noticia.objects.all()
+    # noticias = serializers.serialize('json', Noticia.objects.all())
     return render(request, 'noticias/listar.html', {'noticias': noticias})
